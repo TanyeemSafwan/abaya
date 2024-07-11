bedea3e2b86eeae8c97f7bf161238f29292385b2
TanyeemSafwan <tanyeemsafwan123@gmail.com>
2024-07-11 20:16:36 +0600

    app initialised with authenticaiton and general setup

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+<template>
+  <Navbar />
+  <Sidebar />
+  <slot />
+</template>
