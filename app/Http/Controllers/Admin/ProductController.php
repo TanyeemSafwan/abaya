4306fda5944eca14154bfe5dac5739535538f0dd
TanyeemSafwan <tanyeemsafwan123@gmail.com>
2024-07-13 15:00:53 +0600

    feat(Products):added product update and delete with some design change

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 {
     public function index() 
     {
-        $products = Product::get();
+        $products = Product::with('category', 'brand', 'product_images')->get();
         $brands = Brand::get();
         $categories= Category::get();
 
@@ -42,6 +42,7 @@ class ProductController extends Controller
 
         if($request->hasFile('product_images')) 
         {
+            $productImages = $request->file('product_images');
             foreach ($productImages as $image) {
                 //generate a unique name
                 $uniqueName = time() . '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
@@ -55,5 +56,53 @@ class ProductController extends Controller
             }
         }
         return redirect()->route('admin.products.index')->with('success', 'Product created successfully!');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        if ($request->hasFile('product_images')) {
+            $productImages = $request->file('product_images');
+            // Loop through each uploaded image
+            foreach ($productImages as $image) {
+                // Generate a unique name for the image using timestamp and random string
+                $uniqueName = time() . '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
+
+                // Store the image in the public folder with the unique name
+                $image->move('product_images', $uniqueName);
+
+                // Create a new product image record with the product_id and unique name
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueName,
+                ]);
+            }
+        }
+
+        $product->update();
+        return redirect()->route('admin.products.index')->with('success', 'Product updated successfully.');
+        
+
+    }
+
+    public function deleteImage($id) 
+    {
+        $image = ProductImage::where('id', $id)->delete();
+        return redirect()->route('admin.products.index')->with('success', 'Image deleted successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id)->delete();
+
+        return redirect()->route('admin.products.index')->with('success', 'Product deleted successfully.');
+
     }
 }
